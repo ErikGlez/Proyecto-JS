f00c7e5ab4a1e26bce34891e316b10da54b883d9
erikgonzalez <erik.gonzalez.ac@gmail.com>
2019-09-18 13:31:55 -0700

    Formulario LS
    Formulario falso con LocalStorage y cambios en los post

--- js/main.js
+++ js/main.js
@@ -9,25 +9,21 @@ $(document).ready(function () {
     //post
     var post = [
         {
-            titulo: 'Titulo 1',
+            titulo: 'Commissions',
             date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " de " + moment().format("YYYY"),
-            content: 'orem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti harum velit veritatis Veritatis iste deserunt quod perspiciatis sit Eligendi voluptate impedit expedita dolorum voluptatem ut aliquid magnam accusamus nisi doloribus.'
+            content: 'Actualmente las comisiones se encuentran abiertas y los precios varian segun el tipo de dibujo, por ejemplo: Headshot - 6 USD | Bust 8 USD | Half body 10 USD | Full Body 20 USD. <br> Los metodos de pago son PayPal o Bancoppel (esta es otra opcion para los que son de México). <br> Puedo dibujar: FanArt, OCs (original characters / personajes originales de otros artistas), humanos, animales y personajes de fantasía. No dibujo gore, furry, armaduras, mecha. <br> <a href="https://www.instagram.com/p/B2R2SrwlH6v/" target="_blank">clic para mas detalles.</a>  '
         },
         {
-            titulo: 'Titulo 2',
+            titulo: 'ArtTrade',
             date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " de " + moment().format("YYYY"),
-            content: 'orem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti harum velit veritatis Veritatis iste deserunt quod perspiciatis sit Eligendi voluptate impedit expedita dolorum voluptatem ut aliquid magnam accusamus nisi doloribus.'
+            content: 'Un arttrade es cuando un artista hace un intercambio de dibujo con otro, puede ser por ejemplo el mas comun cuando cada artista dibuja un oc del otro. Esto sirve para que los seguidores de cada artista conoscan al otro artista. Actualmente llevo hechos 2 arttrades, pueden verlos visitando mi perfil en la historia destacada de ArtTrade o clic en el siguiente enlace: <br> <a href="https://www.instagram.com/stories/highlights/17859729613385960/" target="_blank">ArtTrades.</a> '
         },
         {
-            titulo: 'Titulo 3',
+            titulo: 'Art Style',
             date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " de " + moment().format("YYYY"),
-            content: 'orem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti harum velit veritatis Veritatis iste deserunt quod perspiciatis sit Eligendi voluptate impedit expedita dolorum voluptatem ut aliquid magnam accusamus nisi doloribus.'
+            content: 'Aun me falta mucho por pulir mi estilo, el estilo es muy importante ya que es con lo que las personas te pueden identificar con el simple hecho de ver el dibujo. Mi estilo actual aunque no esta muy definido, va muy influenciado por el estilo anime, con un lineart algo limpio. Como mensiono aun falta mucho por mejorar.'
         },
-        {
-            titulo: 'Titulo 4',
-            date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " de " + moment().format("YYYY"),
-            content: 'orem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti harum velit veritatis Veritatis iste deserunt quod perspiciatis sit Eligendi voluptate impedit expedita dolorum voluptatem ut aliquid magnam accusamus nisi doloribus.'
-        },
+       
 
     ]
 
@@ -70,6 +66,31 @@ $(document).ready(function () {
 
          return false;
     });
+
+    //login falso
+
+    $("#login form").submit(function(){
+       var form_name= $("#form_name").val();
+
+       localStorage.setItem("form_name", form_name);
+
+    });
+
+    var form_name = localStorage.getItem("form_name");
+    if(form_name != null && form_name != "undefined"){
+        var about_p = $("#about p");
+        about_p.html("Bienvenido, "+form_name);
+        $("#plogout").html("<a href'#' id='logout'>Cerrar sesion</a>");
+        $("#login").hide();
+        $("#logout").click(function(){
+            localStorage.clear();
+            location.reload();
+        });
+    }else{
+        $("#about p").hide();
+    }
+   
+
 
 
 
